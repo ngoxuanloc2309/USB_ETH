5e330853c6d586a1c7a3f0b58f96e76bc16b671d
ngoxuanloc2309 <ngoxuanloc23092004@gmail.com>
2026-07-02 17:21:44 +0700

    add config IP

--- USB_ETH_SDK/board/board.h
+++ USB_ETH_SDK/board/board.h
@@ -23,6 +23,28 @@ extern "C" {
 #define MAC_BYTE5  0x00
 #define MAC_BYTE6  0x00
 
+#define IP_BYTE1   192
+#define IP_BYTE2   168
+#define IP_BYTE3   137
+#define IP_BYTE4   2
+
+#define GW_BYTE1   192
+#define GW_BYTE2   168
+#define GW_BYTE3   137
+#define GW_BYTE4   1
+
+#define DNS_BYTE1  GW_BYTE1
+#define DNS_BYTE2  GW_BYTE2
+#define DNS_BYTE3  GW_BYTE3
+#define DNS_BYTE4  GW_BYTE4
+
+#define NETMASK_BYTE1  255
+#define NETMASK_BYTE2  255
+#define NETMASK_BYTE3  255
+#define NETMASK_BYTE4  0
+
+
+
 void board_init(void);
 
 void uart_log_write(const char *str);
