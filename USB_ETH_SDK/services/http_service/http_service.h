9226fa9b0fe536deb443a32c7d01f0c7ab676ce5
ngoxuanloc2309 <ngoxuanloc23092004@gmail.com>
2026-07-02 17:17:40 +0700

    add service

---
+++ USB_ETH_SDK/services/http_service/http_service.h
@@ -0,0 +1,12 @@
+#ifndef __HTTP_SERVICE_H__
+#define __HTTP_SERVICE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __HTTP_SERVICE_H__ */
---
+++ USB_ETH_SDK/services/mqtt_service/mqtt_service.h
@@ -0,0 +1,12 @@
+#ifndef __MQTT_SERVICE_H__
+#define __MQTT_SERVICE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MQTT_SERVICE_H__ */
