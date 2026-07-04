7935141aede5b142cb5d55a61afd00f5997a65ae
ngoxuanloc2309 <ngoxuanloc23092004@gmail.com>
2026-07-04 20:03:56 +0700

    .

--- USB_ETH_SDK/app/app_config.h
+++ USB_ETH_SDK/app/app_config.h
@@ -14,12 +14,13 @@ extern "C" {
 #define USB_TASK_PRIORITY       osPriorityAboveNormal
 
 // MQTT config
-#define MQTT_HOST               "3.123.214.148"
-#define MQTT_PORT               1883
-#define MQTT_TEST_PUB_TOPIC     "log/usb_eth"
+#define MQTT_HOST                   "broker.hivemq.com"
+#define MQTT_PORT                    1883
+#define MQTT_TEST_PUB_TOPIC         "log/pub/usb_eth"
+#define MQTT_TEST_SUB_TOPIC         "log/sub/usb_eth"
 #define MQTT_APP_CLIENT_ID          "usb_eth_dev_01"
-#define MQTT_APP_KEEPALIVE_S        60
-#define MQTT_APP_PUBLISH_PERIOD_MS  10000
+#define MQTT_APP_KEEPALIVE_S         60
+#define MQTT_APP_PUBLISH_PERIOD_MS   10000
 
 
 #ifdef __cplusplus
